d58e61aac656894709de2f5637a7127e07d5b6ef
gjsl0710 <gjsl0710@ruu.kr>
2020-10-16 12:37:51 +0900

    add

--- index.js
+++ index.js
@@ -217,6 +217,8 @@ if(message.content == '문아 문이봇초대') {
       .setFooter('Made By ! MOON', img)
 
     message.channel.send(embed)
+
+  
   } else if(message.content == '문아 도움말') {
     let helpImg = 'https://cdn.discordapp.com/attachments/763603688476377088/766481577172795452/G3c1.gif';
     let commandList = [
